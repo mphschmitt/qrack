1c80af6a4b0c8aa96bed7c55fbdf31f66f300dd9
Daniel Strano <stranoj@gmail.com>
2021-10-09 07:39:57 -0400

    Fix pureqrack name

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -4,19 +4,11 @@ if (NOT pybind11_FOUND)
 endif()
 
 if (ENABLE_PYBIND11)
-    add_library (pureqrack SHARED
+    pybind11_add_module(pureqrack SHARED
         src/pureqrack.cpp
         )
 
-    execute_process(COMMAND "echo $(python3-config --extension-suffix)" OUTPUT_VARIABLE PYBIND11_EXTENSION)
-    set_target_properties(pureqrack PROPERTIES
-        SUFFIX "${PYBIND11_EXTENSION}" )
-    set_target_properties (pureqrack PROPERTIES
-        VERSION ${PROJECT_VERSION}
-        )
-
-    target_link_libraries (pureqrack ${QRACK_LIBS})
-    target_include_directories(pureqrack PRIVATE ${pybind11_INCLUDE_DIRS})
+    target_link_libraries (pureqrack PRIVATE ${QRACK_LIBS})
     target_compile_options (pureqrack PUBLIC ${QRACK_COMPILE_OPTS})
     target_compile_definitions(pureqrack PUBLIC -DDLL_EXPORTS)
 endif (ENABLE_PYBIND11)
