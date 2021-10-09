0d43c0d42b7cb3ffd8d6013000991811ae965e22
Daniel Strano <stranoj@gmail.com>
2021-10-08 23:05:52 -0400

    vm6502q/pyqrack#1: Partial pybind11 bindings (with exception handling)

--- src/pureqrack.cpp
+++ src/pureqrack.cpp
@@ -17,4 +17,23 @@ PYBIND11_MODULE(pureqrack, m)
     // pybind11::register_exception<std::invalid_argument>(module, "QrackInvalidArgumentException");
 
     m.doc() = "Qrack pybind11 module";
+
+    m.def("init", &init, "");
+    m.def("init_count", &init_count, "");
+    m.def("init_count_type", &init_count_type, "");
+    m.def("init_clone", &init_clone, "");
+    m.def("destroy", &destroy, "");
+    m.def("seed", &seed, "");
+    m.def("set_concurrency", &set_concurrency, "");
+    m.def("Prob", &Prob, "");
+    m.def("PermutationExpectation", &PermutationExpectation, "");
+    m.def("JointEnsembleProbability", &JointEnsembleProbability, "");
+    m.def("PhaseParity", &PhaseParity, "");
+    m.def("ResetAll", &ResetAll, "");
+    m.def("allocateQubit", &allocateQubit, "");
+    m.def("release", &release, "");
+    m.def("num_qubits", &num_qubits, "");
+    m.def("X", &X, "");
+    m.def("Y", &Y, "");
+    m.def("Z", &Z, "");
 }
